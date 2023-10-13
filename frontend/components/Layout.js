c3ce771c696e485e2458426e9e1e617de5563623
Loïc Lecinni <loic@MacBook-Pro-2.local>
2023-10-13 11:09:13 +0200

    transition ok

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -2,16 +2,56 @@ import { useCookies } from 'react-cookie';
 import Footer from './Footer';
 import Header from './Header';
 import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
+import { PageTransition } from 'next-page-transitions';
+
+const transitionStyles = {
+  enter: {
+    opacity: 0,
+  },
+  enterActive: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 export default function Layout({ children }) {
   const [cookies, setCookie, removeCookie] = useCookies(['user']);
   const router = useRouter();
 
   return (
-    <div className='flex flex-col'>
-      <Header />
-      <main>{children}</main>
-      {router.pathname !== '/' && <Footer />}
-    </div>
+    <PageTransition
+      timeout={500}
+      classNames="page-transition"
+      monkeyPatchScrolling
+      loadingDelay={500}
+      loadingTimeout={{
+        enter: 500,
+        exit: 0,
+      }}
+      loadingClassNames="loading-indicator"
+    >
+      <motion.div
+        key={router.route}
+        initial="enter"
+        animate="enterActive"
+        exit="exit"
+        variants={transitionStyles}
+      >
+        <div className='flex flex-col'>
+          <Header />
+          <main>{children}</main>
+          {/* {router.pathname !== '/' && <Footer />} */}
+        </div>
+      </motion.div>
+    </PageTransition>
   );
 }
